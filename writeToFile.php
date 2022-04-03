d362cbbe5c24404ea1683c895d9163ef69a9dc5f
Faris AL-Otaibi <76238196+farisc0de@users.noreply.github.com>
2022-04-03 11:31:42 +0300

    Create writeToFile.php

---
+++ writeToFile.php
@@ -0,0 +1,9 @@
+<?php
+
+function writeToFile($fileName, $data, $args)
+{
+    $data = call_user_func_array($data, $args);
+    $myfile = fopen($fileName, "w+");
+    fwrite($myfile, $data);
+    fclose($myfile);
+}
